e13d073fad611323d4c7370fd82ba00881ea3ed1
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-16 21:43:48 +0530

    Added Redux to Homescreen

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,40 +1,48 @@
 import React, { useEffect, useState } from 'react'
-import './HomeScreen.css'
 import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import './HomeScreen.css'
 import { Scrollbars } from 'react-custom-scrollbars'
 import { Link } from 'react-router-dom'
+import { listUsers } from '../actions/userAction'
 const HomeScreen = () => {
-  const [user, setUser] = useState([])
-
+  const dispatch = useDispatch()
+  const userList = useSelector((state) => state.userList)
+  const { loading, user, error } = userList
   useEffect(() => {
-    const getdata = async () => {
-      const { data } = await axios.get('https://panorbit.in/api/users.json')
-
-      setUser(data['users'])
-    }
-    getdata()
-  }, [])
+    dispatch(listUsers())
+  }, [dispatch])
 
   return (
     <div className='container'>
       <div className='list-user'>
         <div className='header'>Select an account</div>
-        <div className='users'>
-          <Scrollbars style={{ height: 400 }}>
-            {user.map((item) => (
-              <Link key={item.id} className='link' to='/dashboard'>
-                <div className='item' key={item.id}>
-                  <img
-                    alt='Profile'
-                    src={item.profilepicture}
-                    className='user-image'
-                  />
-                  <li key={item.id}>{item.name}</li>
-                </div>
-              </Link>
-            ))}
-          </Scrollbars>
-        </div>
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
+          <h3>{error}</h3>
+        ) : (
+          <div className='users'>
+            <Scrollbars style={{ height: 400 }}>
+              {user.map((item) => (
+                <Link
+                  key={item.id}
+                  className='link'
+                  to={`/dashboard/${item.id}`}
+                >
+                  <div className='item' key={item.id}>
+                    <img
+                      src={item.profilepicture}
+                      alt='Profile'
+                      className='user-image'
+                    />
+                    <li key={item.id}>{item.name}</li>
+                  </div>
+                </Link>
+              ))}
+            </Scrollbars>
+          </div>
+        )}
       </div>
     </div>
   )
